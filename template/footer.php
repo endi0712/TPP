469355649fa4e7ec1340cbda029f8679b3791f04
endi0712 <75595137+endi0712@users.noreply.github.com>
2020-12-29 08:35:21 +0700

    Add files via upload

--- template/footer.php
+++ template/footer.php
@@ -1,101 +1,66 @@
-<div class="footer-top-area">
-        <div class="zigzag-bottom"></div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3 col-sm-6">
-                    <div class="footer-about-us">
-                        <h2>E-UKM Kalurahan Gedangrejo</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis sunt id doloribus vero quam laborum quas alias dolores blanditiis iusto consequatur, modi aliquid eveniet eligendi iure eaque ipsam iste, pariatur omnis sint! Suscipit, debitis, quisquam. Laborum commodi veritatis magni at?</p>
-                        <div class="footer-social">
-                            <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
-                            <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
-                            <a href="#" target="_blank"><i class="fa fa-youtube"></i></a>
-                            <a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="col-md-3 col-sm-6">
-                    <div class="footer-menu">
-                        <h2 class="footer-wid-title">User Navigation </h2>
-                        <ul>
-                            <li><a href="#">My account</a></li>
-                            <li><a href="#">Order history</a></li>
-                            <li><a href="#">Wishlist</a></li>
-                            <li><a href="#">Vendor contact</a></li>
-                            <li><a href="#">Front page</a></li>
-                        </ul>                        
-                    </div>
-                </div>
-                
-                <div class="col-md-3 col-sm-6">
+<footer class="footer">
+		<div class="mt-auto">
+			<div class="row">
+				<div class="col-lg-4 col-md-12 col-sm-12">
+					<img src="assets/img/gkk.png"  width="70" class="mx-auto d-block">
+					<h6>E-UKM Kalurahan Gedangrejo</h6>
+				</div>
+				<div class="col-md-3 col-sm-6">
                     <div class="footer-menu">
                         <h2 class="footer-wid-title">Categories</h2>
                         <ul>
-                            <li><a href="#">Mobile Phone</a></li>
-                            <li><a href="#">Home accesseries</a></li>
-                            <li><a href="#">LED TV</a></li>
-                            <li><a href="#">Computer</a></li>
-                            <li><a href="#">Gadets</a></li>
+                            <li><a href="#">Kerajinan Batik</a></li>
+                            <li><a href="#">Kerajinan Ukir Batu</a></li>
+                            <li><a href="#">Kerajinan Ukir Kayu</a></li>
+                            <li><a href="#">Kerajinan Tas</a></li>
+                            <li><a href="#">Kerajinan Anyaman</a></li>
                         </ul>                        
                     </div>
-                </div>
-                
-                <div class="col-md-3 col-sm-6">
-                    <div class="footer-newsletter">
-                        <h2 class="footer-wid-title">Newsletter</h2>
-                        <p>Sign up to our newsletter and get exclusive deals you wont find anywhere else straight to your inbox!</p>
-                        <div class="newsletter-form">
-                            <form action="#">
-                                <input type="email" placeholder="Type your email">
-                                <input type="submit" value="Subscribe">
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> <!-- End footer top area -->
-    
-    <div class="footer-bottom-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="copyright">
-                        <p>&copy; 2020 E-UKM Kalurahan Gedangrejo. All Rights Reserved. 
-                    </div>
-                </div>
-                
-                <div class="col-md-4">
-                    <div class="footer-card-icon">
-                        <i class="fa fa-cc-discover"></i>
-                        <i class="fa fa-cc-mastercard"></i>
-                        <i class="fa fa-cc-paypal"></i>
-                        <i class="fa fa-cc-visa"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> <!-- End footer bottom area -->
-   
-    <!-- Latest jQuery form server -->
-    <script src="https://code.jquery.com/jquery.min.js"></script>
-    
-    <!-- Bootstrap JS form CDN -->
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    
-    <!-- jQuery sticky menu -->
-    <script src="assets/js/owl.carousel.min.js"></script>
-    <script src="assets/js/jquery.sticky.js"></script>
-    
-    <!-- jQuery easing -->
-    <script src="assets/js/jquery.easing.1.3.min.js"></script>
-    
-    <!-- Main Script -->
-    <script src="assets/js/main.js"></script>
-    
-    <!-- Slider -->
-    <script type="text/javascript" src="assets/js/bxslider.min.js"></script>
-	<script type="text/javascript" src="assets/js/script.slider.js"></script>
-  </body>
+                </div> 
+				<div class="col-lg-4 col-md-12 col-sm-12 contact-footer" >
+					<h4>Contact Us</h4>
+					<a class="btn" href="#">
+			        	<i class="fab fa-facebook-f"></i>
+			     	</a>
+			     	<a class="btn" href="#">
+			        	<i class="fab fa-twitter"></i>
+			      	</a>
+			   
+			      	<a class="btn" href="#">
+			       	 <i class="fab fa-instagram"></i>
+		     		</a>
+		     		<br>
+		     
+
+		     		<p><i class="fa fa-map-marker"></i> Gedangan III,Gedangrejo,Karangmojo 55891</p>
+		     	
+		     		<p><i class="fa fa-phone"></i> (0274) 654-123987</p>
+		     		
+		     		<p><i class="fa fa-envelope"></i> e-ukm@gmail.com</p>
+		     		
+				</div>
+				<div class="col-lg-12 col-md-12 mt-2">
+						<p class="ftex">&copy; 2020 E-UKM Kalurahan Gedangrejo. All Rights Reserved. Designed By <b>Endi Kurniawan</b> with  &#9829</p> 
+					</div>
+
+			</div>
+		</div>
+	</footer>
+	
+	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+	<script type="text/javascript">
+		function success(){
+			swal("Good job!", "Sukses Ditambahkan ke Keranjang!", "success");
+		}
+	</script>
+
+
+	<script type="text/javascript" src="assets/js/script.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+</body>
 </html>
--- template/header.php
+++ template/header.php
@@ -1,85 +1,109 @@
-<?php 
-session_start();
-include "lib/koneksi.php";
-include "lib/config.php";
-?>
 <!DOCTYPE html>
-<!--
-	ustora by freshdesignweb.com
-	Twitter: https://twitter.com/freshdesignweb
-	URL: https://www.freshdesignweb.com/ustora/
--->
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>E-UKM</title>
-    
-    <!-- Google Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
-    
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-    
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="assets/css/owl.carousel.css">
-    <link rel="stylesheet" href="assets/style.css">
-    <link rel="stylesheet" href="ssets/css/responsive.css">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	
+	
+	<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="assets/css/magnific-popup.css">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-   
-    <div class="header-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="user-menu">
-                        <ul>
-                            <li><a href="daftar.php"><i class="fa fa-user"></i> Daftar</a></li>
-                            <li><a href="#"><i class="fa fa-user"></i> My Account</a></li>
-                            <li><a href="#"><i class="fa fa-heart"></i> Wishlist</a></li>
-                            <li><a href="keranjang.php"><i class="fa fa-user"></i> My Cart</a></li>
-                            <li><a href="selesai.php"><i class="fa fa-user"></i> Checkout</a></li>
-                            <li><a href="login.php"><i class="fa fa-user"></i> Login</a></li>
-                        </ul>
-                    </div>
-                </div>
-                
-                <div class="col-md-4">
-                    <div class="header-right">
-                        <ul class="list-unstyled list-inline">
-                            <li class="dropdown dropdown-small">
-                                <a data-toggle="dropdown" data-hover="dropdown" class="dropdown-toggle" href="#"><span class="key">currency :</span><span class="value">USD </span><b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">USD</a></li>
-                                    <li><a href="#">INR</a></li>
-                                    <li><a href="#">GBP</a></li>
-                                </ul>
-                            </li>
+	<link rel="stylesheet" href="assets/css/bootstrap-datepicker.css">
+	<link rel="stylesheet" href="assets/css/jquery.timepicker.css">
 
-                            <li class="dropdown dropdown-small">
-                                <a data-toggle="dropdown" data-hover="dropdown" class="dropdown-toggle" href="#"><span class="key">language :</span><span class="value">English </span><b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">English</a></li>
-                                    <li><a href="#">French</a></li>
-                                    <li><a href="#">German</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> <!-- End header area -->
+	<link rel="stylesheet" href="assets/css/flaticon.css">
+	<link rel="stylesheet" href="assets/css/style3.css">
+	<link rel="stylesheet" href="assets/css/style2.css">
+
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css">
+	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+	<title>E-UKM Kalurahan Gedangrejo</title>
+</head>
+<script>    
+	if(typeof window.history.pushState == 'function') {
+		window.history.pushState({}, "Hide", '<?php echo $_SERVER['PHP_SELF'];?>');
+	}
+</script>
+<body>
+
+	<header >
+		<div class="container-fluid p-0" >
+			<nav class="navbar navbar-expand-lg navbar-light navi">
+				<a class="btn" href="#">
+					<i class="fab fa-facebook-f"></i>
+				</a>
+				<a class="btn" href="#">
+					<i class="fab fa-twitter"></i>
+				</a>
+
+				<a class="btn" href="#">
+					<i class="fab fa-instagram"></i>
+				</a>
+
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse menu-button" id="navbarNavDropdown">
+					<div class="mr-auto"></div>
+					<ul class="navbar-nav">
+						<li class="nav-item active">
+							<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="about.php">About</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="kontak.php">Contact</a>
+						</li>
+						<?php session_start() ;if(!empty($_SESSION['idCustomer'])){ ?>
+
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								Account
+							</a>
+							<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+								<a class="dropdown-item" href="profil.php">Profil</a>
+								<a class="dropdown-item" href="riwayat.php">Riwayat</a>
+								<a class="dropdown-item" href="ubah_password.php">Ubah Password</a>
+								<a class="dropdown-item" href="aksi_logout.php">Keluar</a>
+							</div>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="keranjang.php"><i class="fa fa-shopping-cart"></i>  My Cart  <span class="badge badge-warning ml-1"><?php 
+								include "lib/koneksi.php";
+								$idcust = $_SESSION['idCustomer'];
+								$query = mysqli_query($koneksi, "SELECT COUNT(id_keranjang) AS'count' FROM tbl_keranjang WHERE id_customer = $idcust");
+								$jumlah = mysqli_fetch_assoc($query); 
+								echo $jumlah['count'];
+								?></span></a>
+							</li>
+
+							<?php } ; if(empty($_SESSION['idCustomer'])){ ?>
+
+							<form class="form-inline">
+								<div class="btn-login" >
+									<a role="button" href="form_login.php">LOGIN</a>
+								</div>
+
+							</form>
+							<?php } ; ?>
+						</ul>
+					</div>
+				</nav>
+
+
+				<div class="menu" id="navbar">
+					<ul>
+						<li><img src="assets/img/gkk.png"  width="70" class="mx-auto d-block" onclick="location.href='index.php';"></li>
+						<li><a href="shop.php">SHOP PAGES</a></li> 
+					</ul>
+				</div>
+
+
+			</header>
