a13687960b3c9c9513a9f16c7daeed85242d6770
endi0712 <75595137+endi0712@users.noreply.github.com>
2020-12-29 08:45:16 +0700

    Delete selesai.php

--- selesai.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include "template/header.php";
-if(!empty($_SESSION['idUser'])){
-	include "pages/checkout.php";
-	include "template/footer.php";
-}else{
-	header("location:login.php");
-}
-?>
