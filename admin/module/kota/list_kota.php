f76f568261fbfd56da524024840e160ed88aa049
endi0712 <75595137+endi0712@users.noreply.github.com>
2021-01-01 18:00:56 +0700

    Update list_kota.php

--- admin/module/kota/list_kota.php
+++ admin/module/kota/list_kota.php
@@ -1,5 +1,4 @@
-
-        <!--**********************************
+<!--**********************************
             Content body start
             ***********************************-->
             <div class="content-body">
@@ -28,7 +27,13 @@
                                     <?php 
                                     include "../lib/config.php";
                                     include "../lib/koneksi.php";
-                                    $kueriKota = mysqli_query($koneksi, "select * from tbl_kota");
+                                    $halaman = 5;
+                                    $page = isset($_GET["halaman"]) ? (int)$_GET["halaman"] : 1;
+                                    $mulai = ($page>1) ? ($page * $halaman) - $halaman : 0;
+                                    $result = mysqli_query($koneksi,"SELECT * FROM tbl_kota");
+                                    $total = mysqli_num_rows($result);
+                                    $pages = ceil($total/$halaman);  
+                                    $kueriKota = mysqli_query($koneksi, "select * from tbl_kota limit $mulai, $halaman");
                                     while ($kot=mysqli_fetch_array($kueriKota)) {
                                      
                                      ?>
@@ -43,6 +48,25 @@
                                      </tr>
                                 <?php  } ?>
                             </table>
+                            <nav aria-label="Page navigation" style="margin-left: 35%">
+                              <ul class="pagination justify-content-right">
+
+                                <li class="page-item <?php if ($page == 1) {echo 'disabled';}?>">
+                                  <a class="page-link" href="?module=kota&halaman=<?php echo $page - 1; ?>" tabindex="-1" aria-disabled="true">Previous</a>
+                                </li>
+
+                                <?php 
+                                for ($i=1; $i<=$pages ; $i++){
+                                  ?>
+                                  <li class="page-item <?php if ($i == $page) {echo 'disabled';}?> "><a class="page-link" href="?module=kota&halaman=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+                                <?php } ?>
+
+                                <li class="page-item <?php if ($page == $pages) {echo 'disabled';}?>">
+                                  <a class="page-link" href="?module=kota&halaman=<?php echo $page + 1; ?>">Next</a>
+                                </li>
+
+                              </ul>
+                            </nav>
                             <div class="box-footer">
                               <a href="<?php echo $base_url; ?>admin/adminweb.php?module=tambah_kota"><button class="btn btn-primary ">Tambah Kota</button></a>
                           </div>
