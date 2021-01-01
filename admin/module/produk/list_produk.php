bfc2fd6173d4e95197bc753a10f49674debf0530
endi0712 <75595137+endi0712@users.noreply.github.com>
2021-01-01 17:52:11 +0700

    Update list_produk.php

--- admin/module/produk/list_produk.php
+++ admin/module/produk/list_produk.php
@@ -1,5 +1,4 @@
-
-        <!--**********************************
+<!--**********************************
             Content body start
             ***********************************-->
             <div class="content-body">
@@ -29,13 +28,20 @@
                           <th>Harga</th>
                           <th style="width: 110px">Aksi</th>
                         </tr>
+                        
                         <?php 
-                        include "../lib/config.php";
-                        include "../lib/koneksi.php";
-                        $kueriProduk = mysqli_query($koneksi, "select * from tbl_produk");
-                        while ($pro=mysqli_fetch_array($kueriProduk)) {
-
-                         ?>
+                                    include "../lib/config.php";
+                                    include "../lib/koneksi.php";
+                                    $halaman = 5;
+                                    $page = isset($_GET["halaman"]) ? (int)$_GET["halaman"] : 1;
+                                    $mulai = ($page>1) ? ($page * $halaman) - $halaman : 0;
+                                    $result = mysqli_query($koneksi,"SELECT * FROM tbl_produk");
+                                    $total = mysqli_num_rows($result);
+                                    $pages = ceil($total/$halaman);  
+                                    $kueriProduk = mysqli_query($koneksi, "select * from tbl_produk limit $mulai, $halaman");
+                                    while ($pro=mysqli_fetch_array($kueriProduk)) {
+                                     
+                                     ?>
                          <tr>
                           <td><?php 
                           include "../lib/koneksi.php"; 
@@ -59,6 +65,26 @@
                         </tr>
                       <?php  } ?>
                     </table>
+                    <nav aria-label="Page navigation" style="margin-left: 35%">
+                              <ul class="pagination justify-content-right">
+
+                                <li class="page-item <?php if ($page == 1) {echo 'disabled';}?>">
+                                  <a class="page-link" href="?module=produk&halaman=<?php echo $page - 1; ?>" tabindex="-1" aria-disabled="true">Previous</a>
+                                </li>
+
+                                <?php 
+                                for ($i=1; $i<=$pages ; $i++){
+                                  ?>
+                                  <li class="page-item <?php if ($i == $page) {echo 'disabled';}?> "><a class="page-link" href="?module=produk&halaman=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+                                <?php } ?>
+
+                                <li class="page-item <?php if ($page == $pages) {echo 'disabled';}?>">
+                                  <a class="page-link" href="?module=produk&halaman=<?php echo $page + 1; ?>">Next</a>
+                                </li>
+
+                              </ul>
+                            </nav>
+
                     <div class="box-footer">
                       <a href="<?php echo $base_url; ?>admin/adminweb.php?module=tambah_produk"><button class="btn btn-primary ">Tambah Produk</button></a>
                     </div>
