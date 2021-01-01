6d434cf82f3a4d79ba0b5a1308f2d00d28c18a90
endi0712 <75595137+endi0712@users.noreply.github.com>
2021-01-01 17:57:12 +0700

    Update list_biaya.php

--- admin/module/biaya/list_biaya.php
+++ admin/module/biaya/list_biaya.php
@@ -1,5 +1,4 @@
-
-        <!--**********************************
+<!--**********************************
             Content body start
             ***********************************-->
             <div class="content-body">
@@ -24,14 +23,20 @@
                                     <tr>
                                         <th>Nama kota</th>
                                         <th>Nama provinsi</th>
-                                        <th>Nama kurir</th>
+                                        <th>Nama kurir/kargo</th>
                                         <th>Biaya</th>
                                         <th style="width: 110px">Aksi</th>
                                     </tr>
                                     <?php 
                                     include "../lib/config.php";
                                     include "../lib/koneksi.php";
-                                    $kueriBiaya = mysqli_query($koneksi, "select * from tbl_biaya_kirim");
+                                    $halaman = 5;
+                                    $page = isset($_GET["halaman"]) ? (int)$_GET["halaman"] : 1;
+                                    $mulai = ($page>1) ? ($page * $halaman) - $halaman : 0;
+                                    $result = mysqli_query($koneksi,"SELECT * FROM tbl_biaya_kirim");
+                                    $total = mysqli_num_rows($result);
+                                    $pages = ceil($total/$halaman);  
+                                    $kueriBiaya = mysqli_query($koneksi, "select * from tbl_biaya_kirim limit $mulai, $halaman");
                                     while ($bia=mysqli_fetch_array($kueriBiaya)) {
                                      
                                      ?>
@@ -70,6 +75,25 @@
                                      </tr>
                                 <?php  } ?>
                             </table>
+                            <nav aria-label="Page navigation" style="margin-left: 35%">
+                              <ul class="pagination justify-content-right">
+
+                                <li class="page-item <?php if ($page == 1) {echo 'disabled';}?>">
+                                  <a class="page-link" href="?module=biaya&halaman=<?php echo $page - 1; ?>" tabindex="-1" aria-disabled="true">Previous</a>
+                                </li>
+
+                                <?php 
+                                for ($i=1; $i<=$pages ; $i++){
+                                  ?>
+                                  <li class="page-item <?php if ($i == $page) {echo 'disabled';}?> "><a class="page-link" href="?module=biaya&halaman=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+                                <?php } ?>
+
+                                <li class="page-item <?php if ($page == $pages) {echo 'disabled';}?>">
+                                  <a class="page-link" href="?module=biaya&halaman=<?php echo $page + 1; ?>">Next</a>
+                                </li>
+
+                              </ul>
+                            </nav>
                             <div class="box-footer">
                               <a href="<?php echo $base_url; ?>admin/adminweb.php?module=tambah_biaya"><button class="btn btn-primary ">Tambah Biaya</button></a>
                           </div>
